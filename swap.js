9a768f0f74af5c331e572556d8ad250826d67873
NHTanim <nazmulhossaintanim78@gmail.com>
2021-09-20 00:47:50 +0600

    js basic

---
+++ swap.js
@@ -0,0 +1,16 @@
+// 
+
+
+var a = 5;
+var b = 9;
+console.log("Before swap value a=", a, "b=",b);
+// এখানে value change করে মানে swap করে a এর value b, আর b এর value a তে নেয়া হয়েছে temp এর মাধ্যম এ ।
+var temp = a;
+a = b;
+b = temp;
+console.log("After swap value a=", a, "b=", b);
+
+
+
+
+// 
